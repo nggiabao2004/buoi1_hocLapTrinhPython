cd41d251f82a5662f11df2ce59d7d301530dd58e
nggiabao2004 <nguyengiabao022004@gmail.com>
2024-12-25 16:01:53 +0700

    bai13

---
+++ bai13.py
@@ -0,0 +1,15 @@
+h=int(input("Nhap gio:"))
+m=int(input("Nhap phut:"))
+s=int(input("Nhap giay:"))
+
+if 0<=h<=23:
+    if 0<=m<60:
+        if 0<=s<60:
+            print("Thoi gian hop le")
+            print(h,":",m,":",s)
+        else:
+            print("Thoi gian khong hop le")
+    else:
+        print("Thoi gian khong hop le") 
+else:
+    print("Thoi gian khong hop le")       

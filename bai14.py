cc01c49322fec303d5ba64869d11c55dfb6cdbdd
nggiabao2004 <nguyengiabao022004@gmail.com>
2024-12-27 13:17:04 +0700

    fixLogicError_buoi14

--- bai14.py
+++ bai14.py
@@ -1,12 +1,13 @@
 x=int(input("Nhap so km taxi chay duoc: "))
 tongTien=0
-if x<0:
-    print("vui long nhap lai")
+if x<=0:
+    print("vui long nhap lai so km lon hon 0")
 else:
     if x==1:
         tongTien=16
-    else:
-        if 1<x<=30:
-            tongTien= 16+(x-1)*15
-        else:
-            tongTien= 16+29*15+(x-30)*12
+    elif 1<x<=30:
+        tongTien=16+(x-1)*15
+    elif x>30:
+        tongTien=16+29*15+(x-30)*12
+
+    print("Tong tien phai tra la", tongTien)

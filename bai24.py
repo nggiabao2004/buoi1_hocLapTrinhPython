db724d1fe4a7a100afb586e828220e1d03e551e0
nggiabao2004 <nguyengiabao022004@gmail.com>
2025-01-02 09:34:23 +0700

    bai25

---
+++ bai24.py
@@ -0,0 +1,7 @@
+#UocSo(12) = {1; 2; 3; 4; 6; 12}
+#UocSo(15) = {1; 3; 5; 15}
+#BoiSo(12) = {12; 24; 36; 48; 60;...}
+#BoiSo(15) = {15; 30; 45; 60;...}
+
+a=int(input("Nhap so thu nhat: "))
+b=int(input("Nhap so thu hai: "))
---
+++ bai25.py
@@ -0,0 +1,19 @@
+#UocSo(12) = {1; 2; 3; 4; 6; 12}
+#6=s(6)=1+2+3
+n=int(input("Nhap so nguyen: "))
+
+total=0
+if n<=0:
+    print("Nhap so lon hon 0")
+else:
+    for i in range (1, n):
+        if n%i==0:
+            total+=i
+            i+=1
+        else:
+            i+=1
+          
+if total==n:
+    print(n,"la so hoan thien")
+else:
+    print(n,"khong phai la so hoan thien")
